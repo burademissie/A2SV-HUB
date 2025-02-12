c414766c07c02dfd38b68f9cbc8268bd77180352
Biruk Demissie <156226117+burademissie@users.noreply.github.com>
2025-02-12 22:15:14 +0300

    Created 'X-Sum' by A2SV Hub

---
+++ 12-Feb-2025/X-Sum 261372.py
@@ -0,0 +1,38 @@
+# Problem: X-Sum - https://codeforces.com/problemset/problem/1676/D
+
+from collections import defaultdict
+
+
+inp = int(input())
+
+
+
+for i in range(inp):
+    matrix = []
+    rowcol = list(map(int , input().split()))
+    for j in range(rowcol[0]):
+        lis = list(map(int,input().split()))
+        matrix.append(lis)
+    
+    diagonal = defaultdict(int)
+    non = defaultdict(int)
+    
+    
+    for k in range(rowcol[0]):
+        for l in range(rowcol[1]):
+            diagonal[k-l] += matrix[k][l]
+            non[k+l] += matrix[k][l]
+            
+    
+    max_sum = 0 
+    for m in range(rowcol[0]):
+        for n in range(rowcol[1]):
+            
+            # print(max_sum)
+            summ = diagonal[m-n] + non[m+n] - matrix[m][n]
+            
+            max_sum = max(summ,max_sum)
+    print(max_sum)
+            
+    
+    
