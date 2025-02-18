d1f7b9d6655d3f02908b256482c08341ee2a40c2
Biruk Demissie <156226117+burademissie@users.noreply.github.com>
2025-02-18 17:03:11 +0300

    Created 'Largest Number' by A2SV Hub

---
+++ 18-Feb-2025/Largest Number 272005.py
@@ -0,0 +1,19 @@
+# Problem: Largest Number - https://leetcode.com/problems/largest-number/
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        nums = list(map(str,nums))
+
+        def compare(x , y):
+            if x + y > y + x:
+                return -1
+            else:
+                return 1
+        
+        
+        nums.sort(key=cmp_to_key(compare))
+        if nums[0] == '0':
+            return "0"
+
+        nums = "".join(nums)
+        return nums
