afb743a832354b2d3d7387894db7f19d2ecd5c3b
Biruk Demissie <156226117+burademissie@users.noreply.github.com>
2025-02-20 21:40:28 +0300

    Created 'B - Anansi and Trip-Photographs' by A2SV Hub

---
+++ A2SV G6 - Round #3 20-Feb-2025/B - Anansi and Trip-Photographs 277298.py
@@ -0,0 +1,24 @@
+# Problem: B - Anansi and Trip-Photographs - https://codeforces.com/gym/588468/problem/B
+
+test= int(input())
+
+for t in range(test):
+    x , heights = map(int,input().split())
+    heightarr = list(map(int,input().split()))
+    
+    heightarr.sort()
+    
+    
+    
+    front = 0
+    back = x
+    
+    count = 0
+    while front < x:
+        if heightarr[front] + heights <= heightarr[front+back]:
+            count += 1 
+        front += 1
+    if count == x:
+        print("YES")
+    else:
+        print("NO")
