a6c5ba7e20441494059d60df9faa1165e9903be9
Biruk Demissie <156226117+burademissie@users.noreply.github.com>
2025-02-27 13:47:27 +0300

    Created 'D - Minimize the Distance' by A2SV Hub

---
+++ A2SV G6 - Round #4 27-Feb-2025/D - Minimize the Distance 285892.py
@@ -0,0 +1,11 @@
+# Problem: D - Minimize the Distance - https://codeforces.com/gym/590053/problem/D
+
+t = int(input())
+if t%2 == 0:
+    midian = t//2 -1
+else:
+    midian = t//2
+
+nums = list(map(int,input().split()))
+nums.sort()
+print(nums[midian])
