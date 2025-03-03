7a04f9c56e49b5020e055e57226d32ebc450ef91
Biruk Demissie <156226117+burademissie@users.noreply.github.com>
2025-03-04 00:15:35 +0300

    Created 'C - Binary Flip' by A2SV Hub

---
+++ A2SV G6 - Round #4 03-Mar-2025/C - Binary Flip 290314.py
@@ -0,0 +1,25 @@
+# Problem: C - Binary Flip - https://codeforces.com/gym/590053/problem/C
+
+t = int(input())
+
+def solve ():
+    size = int(input())
+    binarya = input()
+    binaryb = input()
+    count = 0
+    binarya += '0'
+    binaryb += '0'
+    
+    for i in range(size):
+        if binarya[i] == '1':
+            count += 1
+        else:
+            count -= 1
+            
+        if ((binarya[i] == binaryb[i]) != (binarya[i + 1] == binaryb[i + 1])) and count != 0:
+            print("NO")
+            return
+    print("YES")
+        
+for _ in range(t):
+    solve()
