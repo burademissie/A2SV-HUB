ab9b8c0ed204696858d78d76937be8386b931e54
Biruk Demissie <156226117+burademissie@users.noreply.github.com>
2025-03-04 00:24:05 +0300

    Created 'C - Escape-Proof Transfers' by A2SV Hub

---
+++ A2SV G6 - Round #5 03-Mar-2025/C - Escape-Proof Transfers 290326.py
@@ -0,0 +1,17 @@
+# Problem: C - Escape-Proof Transfers - https://codeforces.com/gym/591928/problem/C
+
+n , t , c = map(int,input().split())
+
+pri = list(map(int,input().split()))
+
+left = 0
+ans = 0
+
+for right in range(n):
+    if pri[right] > t:
+        left = right + 1
+    
+    currlen = right - left + 1
+    if currlen >= c:
+        ans += 1
+print(ans)
