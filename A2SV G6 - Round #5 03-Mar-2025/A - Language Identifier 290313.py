ab16d05efd029d5dfd25c0be380dfbbbfd393f6f
Biruk Demissie <156226117+burademissie@users.noreply.github.com>
2025-03-04 00:15:25 +0300

    Created 'A - Language Identifier' by A2SV Hub

---
+++ A2SV G6 - Round #5 03-Mar-2025/A - Language Identifier 290313.py
@@ -0,0 +1,13 @@
+# Problem: A - Language Identifier - https://codeforces.com/gym/591928/problem/A
+
+t= int(input())
+
+for _ in range(t):
+    lang = input()
+    
+    if "po" == lang[-2:]:
+        print("FILIPINO")
+    elif "desu" == lang[-4:] or "masu" == lang[-4:]:
+        print("JAPANESE")
+    elif "mnida" == lang[-5:]:
+        print("KOREAN")
