77c19ba9e476e00f4b0338fc0feb40769bce7094
Biruk Demissie <156226117+burademissie@users.noreply.github.com>
2025-03-05 19:08:13 +0300

    Created 'Odd Subarrays' by A2SV Hub

---
+++ 05-Mar-2025/Odd Subarrays 293274.py
@@ -0,0 +1,20 @@
+# Problem: Odd Subarrays - https://codeforces.com/problemset/problem/1686/B
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    
+    arr = list(map(int,input().split()))
+    diff = [0] * n
+    for i in range(n):
+        if i>0:
+            diff[i] = arr[i-1] - arr[i]
+    i = 0
+    ans = 0
+    while i < n:
+        if diff[i] > 0:
+            i += 1
+            ans += 1
+        i += 1
+    print(ans)
