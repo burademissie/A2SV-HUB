8700036935a021df9fb6be383b62d9d7bbbe4126
Biruk Demissie <156226117+burademissie@users.noreply.github.com>
2025-03-05 11:08:21 +0300

    Created 'Red and Blue' by A2SV Hub

---
+++ 05-Mar-2025/Red and Blue 292640.py
@@ -0,0 +1,21 @@
+# Problem: Red and Blue - https://codeforces.com/contest/1469/problem/B
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    r = list(map(int,input().split()))
+    m = int(input())
+    b = list(map(int,input().split()))
+    ansr = 0
+    ansb = 0
+    currsum =0
+    for i in range(n):
+        currsum += r[i]
+        ansr = max(ansr ,currsum)
+    currsum = 0
+    for i in range(m):
+        currsum += b[i]
+        ansb = max(ansb , currsum)
+    
+    print(ansr + ansb)
