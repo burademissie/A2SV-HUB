833dcfb2812bcb3f465eb8e43267e0e7a99c4e1a
Biruk Demissie <156226117+burademissie@users.noreply.github.com>
2025-03-06 23:00:54 +0300

    Created 'D - Life Across the Stars' by A2SV Hub

---
+++ A2SV G6 - Round #5 06-Mar-2025/D - Life Across the Stars 295343.py
@@ -0,0 +1,29 @@
+# Problem: D - Life Across the Stars - https://codeforces.com/gym/591928/problem/D
+
+from collections import defaultdict
+
+t = int(input())
+
+life = defaultdict(int)
+
+for _ in range(t):
+    b , d = map(int,input().split())
+    
+    life[b] += 1
+    life[d] -= 1
+
+keys = sorted(list(life.keys()))
+pre = []
+for i in range(len(keys)):
+    pre.append([keys[i],life[keys[i]]])
+
+for i in range(len(keys)):
+    if i > 0:
+        pre[i][1] += pre[i-1][1] 
+maxn = float('-inf')
+for y , n in pre:
+    
+    if n > maxn:
+        newy = y
+        maxn = n
+print(newy , maxn)
