3a3507984c2db8d65858cd708b69553441e3fe15
Biruk Demissie <156226117+burademissie@users.noreply.github.com>
2025-03-06 11:07:02 +0300

    Created 'Longest k-Good Segment' by A2SV Hub

---
+++ 06-Mar-2025/Longest k-Good Segment 293975.py
@@ -0,0 +1,26 @@
+# Problem: Longest k-Good Segment - https://codeforces.com/problemset/problem/616/D
+
+from collections import defaultdict
+n , k = map(int,input().split())
+
+nums = list(map(int,input().split()))
+
+numset = defaultdict(int)
+right = left = maxlen = 0
+l = r = 0
+while right < n:
+    numset[nums[right]] += 1
+    
+    while len(numset) > k and left < n:
+        numset[nums[left]] -= 1
+        if numset[nums[left]] == 0:
+            del numset[nums[left]]
+        left += 1
+    if right - left > maxlen:
+        l = left
+        r = right
+        
+        maxlen = right - left
+    right += 1
+print(l+1 , r+1)
+        
