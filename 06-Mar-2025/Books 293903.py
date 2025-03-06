f14bda8e68223ad321e46211b4c9406eb684bea1
Biruk Demissie <156226117+burademissie@users.noreply.github.com>
2025-03-06 09:46:35 +0300

    Created 'Books' by A2SV Hub

---
+++ 06-Mar-2025/Books 293903.py
@@ -0,0 +1,16 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+n , t = map(int,input().split())
+
+nums = list(map(int,input().split()))
+
+cursum = 0
+left = maxlen = 0
+for right in range(n):
+    cursum += nums[right]
+    while cursum > t:
+        cursum -= nums[left]
+        left += 1
+    maxlen = max(maxlen , right - left + 1)
+print(maxlen)
+    
